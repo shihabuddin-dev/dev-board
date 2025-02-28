d655a53513e1e4687989b3a830e13986b410a36e
Shihab Uddin <shihabuddin.dev@gmail.com>
2025-03-01 00:49:52 +0600

    i have been doing code and made it by js background color change, and make it functionality

--- js/utilities.js
+++ js/utilities.js
@@ -1,5 +1,23 @@
+// Change random background color 
+function changeBgColor() {
+    const randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
+    document.getElementById("body").style.backgroundColor = randomColor;
+}
 
-    // <!-- JavaScript -->
 
-      
-    
+// Auto day and Date Update
+function updateDate() {
+    const today = new Date();
+    const options = { weekday: 'long', month: 'short', day: 'numeric', year: 'numeric' };
+    const formattedDate = today.toLocaleDateString('en-US', options).split(',');
+
+    document.getElementById('day').innerText = formattedDate[0] + ",";
+    document.getElementById('date').innerText = formattedDate[1] + formattedDate[2];
+}
+updateDate();
+
+// disable buttons 
+function disableButton(button) {
+    button.disabled = true; 
+    button.classList.add("bg-gray-400", "cursor-not-allowed");
+}
